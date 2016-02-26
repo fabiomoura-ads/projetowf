8335dce5bde8dd3c030f97d1d70d6e4b484e4938
= <=>
2016-02-26 14:20:03 -0300

    Criado migration para inclusao do campo profile ( imagem do usuario )

---
+++ database/migrations/2016_02_26_171557_inc_profile_users.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class IncProfileUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->binary('profile');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
