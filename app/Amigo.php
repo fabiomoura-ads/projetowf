6b506085f03bb12f6da615b2b94f4e7864ec077d
fabiomoura.ads <fabiomoura.ads@gmail.com>
2016-02-28 23:54:36 -0300

    Criado classe Amigo

---
+++ app/Amigo.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Amigo extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id', 'user_id', 'amigo_id',
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at', 'updated_at',
+    ]; 
+	
+}
